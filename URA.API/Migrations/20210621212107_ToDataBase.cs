8c5a45b27710e74d19f38eb84ebb2b6d0e9c54d8
Diego Ferreira <diegoof@gmail.com>
2021-06-21 17:23:39 -0400

    Migration added to database

---
+++ URA.API/Migrations/20210621212107_ToDataBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace URA.API.Migrations
+{
+    public partial class ToDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
